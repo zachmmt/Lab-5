21dccc8a54a755892f2d851c50745065e197c724
Zach Miller <zachmillermt@gmail.com>
2018-03-02 12:12:32 -0700

    I doubt this will work

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -19,6 +19,12 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 
 }
